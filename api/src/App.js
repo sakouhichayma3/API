bb23704c3c85c33e4f9b6b98804e4f52c0ec464a
sakouhichayma3 <127346052+sakouhichayma3@users.noreply.github.com>
2023-05-31 20:55:02 +0200

    Update App.js

--- api/src/App.js
+++ api/src/App.js
@@ -3,7 +3,7 @@ import Userliste from './composant/Userliste';
 
 const App = () => {
   return (
-    <div style={{marginTop: "50px", boxSizing:"border-box", borderBlockStyle:"double", backgroundImage: "linear-gradient(pink,lightblue)", padding:"30px",fontsize: "20px", color:"#4a54f1", textalign:"center"}}>
+    <div style={{marginTop: "50px", boxSizing:"border-box", borderBlockStyle:"double", backgroundImage: "linear-gradient(pink,lightblue)", padding:"30px",fontsize: "20px", color:"#4a54f1", textAlign:"center"}}>
       <Userliste/>
     </div>
   );
